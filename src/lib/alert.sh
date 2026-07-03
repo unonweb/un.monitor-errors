1a3fe7669ae50c4ca7e1553862b87d403e63c2c9
unonweb <unonweb@posteo.de>
2026-07-03 10:54:37 +0200

    - add checks - add early returns

--- src/lib/alert.sh
+++ src/lib/alert.sh
@@ -8,18 +8,44 @@ function alert {
 	local subject="${1}"
     local message="${2}"
 
-	if (( ALERT_MAIL )); then
-
-		if [[ -z "${MAIL_TO}" ]]; then
-			log "<3> Required var not set: MAIL_TO"
-			return 1
-		else
-			log "<6> Sending Mail-Alert to ${MAIL_TO}"
-
-			echo -e "${message}" | \
-			mail -s "${MAIL_SUBJECT} ALERT: ${subject}" "${MAIL_TO}" \
-			&& log "<6> Mail-Alert sent to ${MAIL_TO}" \
-			&& return 0
-		fi
+	if (( ! ALERT_MAIL )); then
+		return 0
 	fi
+
+	# CHECK internal deps
+	for fctn in log; do
+    	if ! declare -f "${fctn}" > /dev/null; then
+        	echo "<3> Error: Required function missing: ${fctn}" >&2
+        	return 1
+    	fi
+	done
+
+	if [[ -z "${message}" ]]; then
+		log "<7> No message to alert"
+		return 0
+	fi
+
+	# CHECK external deps
+	for cmd in mail; do
+    	if ! command -v "${cmd}" &> /dev/null; then
+        	log "<3> Error: Required external command missing: ${cmd}" >&2
+        	return 1
+    	fi
+	done
+
+	# CHECK vars
+	for var in ALERT_MAIL MAIL_TO; do
+		if [[ -z "${!var}" ]]; then
+			log "<3> Required var missing: ${var}"
+			return 1
+		fi
+	done
+
+	# ALERT
+	log "<6> Sending Mail-Alert to ${MAIL_TO}"
+
+	echo -e "${message}" | \
+	mail -s "${MAIL_SUBJECT} ALERT: ${subject}" "${MAIL_TO}" \
+	&& log "<6> Mail-Alert sent to ${MAIL_TO}" \
+	&& return 0
 }
